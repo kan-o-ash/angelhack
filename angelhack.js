6bbe4454914d14d39eb625df321bb42db7941ff5
kan-o-ash <ashkan.pk@gmail.com>
2014-06-22 12:59:46 -0400

    done

--- angelhack.js
+++ angelhack.js
@@ -4,6 +4,7 @@ if (Meteor.isClient) {
   // Meteor.subscribe('user-ingredients');
   Meteor.startup(function () {
     Session.set('search_ready', false)
+    Session.set('recip_ready', false)
   })
 
   Template.main.helpers({
@@ -12,6 +13,9 @@ if (Meteor.isClient) {
     },
     searchReady: function () { // Is the search ready to display?
       return Session.get('search_ready');
+    },
+    recipeReady: function () { // Is the search ready to display?
+      return Session.get('recipe_ready');
     },
     addIngredient: function (field) {
 
@@ -46,18 +50,32 @@ if (Meteor.isClient) {
       Template.main.addIngredient(field);
     },
     'click #submit_btn': function () { // Clicking submit to get recipes
-      Meteor.call('getRecipe', Template.ingr.foodItems, function(error, response) {
-        if (response) {
-          var recipes = JSON.parse(response.content);
-          Session.set('search_results', recipes); // Dictionary of recipes
-          Session.set('search_ready', true);      // Makes search results display
-          console.log(recipes);
-        }
-        else {
-          console.log(error);
-          console.log(2);
-        }
-      });
+      if(Template.ingr.foodItems.length) {
+        Meteor.call('getRecipe', Template.ingr.foodItems, function(error, response) {
+          if (response) {
+            Session.set('search_results', response); // Dictionary of recipes
+            Session.set('search_ready', true);      // Makes search results display
+            console.log(response);
+          }
+          else {
+            console.log(error);
+            console.log(2);
+          }
+        });
+      }
+    },
+    'click #back_btn': function () {
+      if (Session.get('search_ready')) {
+        Session.set('search_ready', false);      // Makes search results display      
+        Session.set('recipe_ready', false);
+      }
+      else if (Session.get('recipe_ready')) {
+        Session.set('search_ready', true);      // Makes search results display      
+        Session.set('recipe_ready', false);
+      }
+    },
+    'click #home_btn': function () {
+      location.reload();
     }
   });
 
@@ -67,10 +85,25 @@ if (Meteor.isClient) {
   
   Template.searchResults.helpers({
     results: function () {
-      console.log(Session.get('search_results').matches);
-      return Session.get("search_results").matches;
+      return Session.get("search_results");
     }
   });
+  Template.searchResults.events({
+    'click .item': function () {
+      console.log(event);
+      $item = $(event.target);
+      var id = $item.attr('arrayID');
+      var recipes = Session.get('search_results');
+      Session.set('recipe_item', recipes[id]);
+      Session.set('recipe_ready', true);
+      Session.set('search_ready', false);
+    }
+  });
+  Template.recipeInfo.helpers({
+    recipeItem: function () {
+      return Session.get('recipe_item');
+    }
+  })
 }
 
 
@@ -91,7 +124,7 @@ if (Meteor.isServer) {
     // addIngredient: function (ingredient, userId) {
     //   Ingredients.insert({name: ingredient, user_id: userId});
     // },
-    buildURL: function ( ingredients) {
+    buildURL: function (ingredients) {
       url = BASE_URL + "?";
       for (var i=0; i<ingredients.length; i++) {
         if (i !== 0) {
@@ -105,7 +138,8 @@ if (Meteor.isServer) {
       var params = {
         '_app_key': APP_KEY,
         '_app_id': APP_ID,
-        'maxResult': 10
+        'maxResult': 16,
+        'requirePictures': true
       };
 
       //url_call = url + "?app_key=" + _app_key + "&_app_id=" + _app_id + "&allowedIngredient[]=cognac";
@@ -116,11 +150,21 @@ if (Meteor.isServer) {
       this.unblock();
       try {
         var results = HTTP.call("GET", url, {params: params});
-        //var result = HTTP.get("http://api.yummly.com/v1/api/recipes" + "?_app_key=" + params._app_key + "&_app_id=" + params._app_id + "&allowedIngredient[]=cognac");  
-        // var results =  HTTP.get("http://api.yummly.com/v1/api/recipes?_app_key=f417e2ff0264769fc557b116fb4197d0&_app_id=b1229d17&allowedIngredient[]=cognac");
-        // console.log(results);
-        //result = 'test'
-        return results;
+        
+        var recipes = JSON.parse(results.content).matches;
+
+        var new_rep = [];
+        for (var i=0; i<recipes.length;i++){
+          values = {
+            'name': recipes[i].recipeName,
+            'src': recipes[i].smallImageUrls[0],
+            'ingredients': recipes[i].ingredients,
+            'timeTaken': recipes[i].totalTimeInSeconds,
+            'ind': i
+          }
+          new_rep.push(values);
+        }
+        return new_rep;
       } catch (e) {
         // Got a network error, time-out or HTTP error in the 400 or 500 range.
         return false;
