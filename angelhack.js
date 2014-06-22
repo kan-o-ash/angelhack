fc9fdca79939512f79f852bbd45292bbc3627f45
kan-o-ash <ashkan.pk@gmail.com>
2014-06-22 08:31:37 -0400

    A lot of shit

--- angelhack.js
+++ angelhack.js
@@ -1,76 +1,126 @@
-APP_KEY = "f417e2ff0264769fc557b116fb4197d0";
-APP_ID = "b1229d17";
+
 
 if (Meteor.isClient) {
   // Meteor.subscribe('user-ingredients');
+  Meteor.startup(function () {
+    Session.set('search_ready', false)
+  })
 
-  Template.input.greeting = function () {
-    return "munchies.";
-  };
+  Template.main.helpers({
+    greeting: function () { // The top greeting of site
+      return "munchies.";
+    },
+    searchReady: function () { // Is the search ready to display?
+      return Session.get('search_ready');
+    },
+    addIngredient: function (field) {
 
-  // Template.input.foodItems = function () {
-  //   return Ingredients.find();
-  // }
+      //The ingredient enterred
+      var ingr = field.value;
 
-  Template.ingr.foodItems = [];
+      // If already in the list, don't bother with it
+      if (Template.ingr.foodItems.indexOf(ingr) !== -1 ) {
+        field.value = "";
+      }
+      // If new, valid ingredient
+      else if (ingr) {
+        Template.ingr.foodItems.push(ingr)
+        field.value = "";
+        var node = $("#food_items");
+        node[0].innerHTML = "";
+        var temp = UI.render(Template.ingr);
+        UI.insert(temp, node[0]);
+      }
+    }
+  });
 
-  Template.input.events({
+  Template.main.events({
     'keyup #ingredient_box': function () {
-      if (event.keyCode == 13) {
-        var ingr = event.target.value;
-        if (ingr) {
-          Template.ingr.foodItems.push({'name': ingr})
-          event.target.value = "";
-          var node = document.getElementById("food-items");   
-          node.innerHTML ="";
-          var temp = UI.render(Template.ingr);
-          UI.insert(temp, node);
-        }
+      if (event.keyCode === 13) {
+        Template.main.addIngredient($(event.target)[0]);
       }
     },
-    'click #submit': function () {
+    'click #add_ingredient':  function () {
+      var field = $('#ingredient_box')[0]
+      field.focus();
+      Template.main.addIngredient(field);
+    },
+    'click #submit_btn': function () { // Clicking submit to get recipes
       Meteor.call('getRecipe', Template.ingr.foodItems, function(error, response) {
-
+        if (response) {
+          var recipes = JSON.parse(response.content);
+          Session.set('search_results', recipes); // Dictionary of recipes
+          Session.set('search_ready', true);      // Makes search results display
+          console.log(recipes);
+        }
+        else {
+          console.log(error);
+          console.log(2);
+        }
       });
     }
   });
-}
 
+  Template.ingr.helpers({
+    foodItems: []
+  });
+  
+  Template.searchResults.helpers({
+    results: function () {
+      console.log(Session.get('search_results').matches);
+      return Session.get("search_results").matches;
+    }
+  });
+}
 
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     //if (Ingredients.find().count() === 0) {
     //}
+    APP_KEY = "f417e2ff0264769fc557b116fb4197d0";
+    APP_ID = "b1229d17";
+    BASE_URL = "http://api.yummly.com/v1/api/recipes";
+    ingredient_arg = "allowedIngredient[]";
+    // if (!sample) {
+      // sample = HTTP.get("http://api.yummly.com/v1/api/recipes?_app_key=f417e2ff0264769fc557b116fb4197d0&_app_id=b1229d17&allowedIngredient[]=cognac").content;
+    // }
   });
-
-
 
   Meteor.methods({
     // addIngredient: function (ingredient, userId) {
     //   Ingredients.insert({name: ingredient, user_id: userId});
     // },
-
+    buildURL: function ( ingredients) {
+      url = BASE_URL + "?";
+      for (var i=0; i<ingredients.length; i++) {
+        if (i !== 0) {
+          url += "&";
+        }
+        url += ingredient_arg + "=" + ingredients[i] 
+      }
+      return url;
+    },
     getRecipe: function (ingredients) {
-
-
       var params = {
         '_app_key': APP_KEY,
         '_app_id': APP_ID,
-        'allowedIngredient[]': "cognac"
+        'maxResult': 10
       };
 
       //url_call = url + "?app_key=" + _app_key + "&_app_id=" + _app_id + "&allowedIngredient[]=cognac";
 
+      url = Meteor.call('buildURL', ingredients);
+
       // Do call here, return value with Future
       this.unblock();
       try {
-        //var result = HTTP.call("GET", "http://api.yummly.com/v1/api/recipes", {params: params});
-        var result = HTTP.get("http://api.yummly.com/v1/api/recipes" + "?_app_key=" + params._app_key + "&_app_id=" + params._app_id + "&allowedIngredient[]=cognac");  
-        // var result = "http://api.yummly.com/v1/api/recipes?_app_key=f417e2ff0264769fc557b116fb4197d0&_app_id=b1229d17&allowedIngredient[]=cognac";
-        console.log(result);
+        var results = HTTP.call("GET", url, {params: params});
+        //var result = HTTP.get("http://api.yummly.com/v1/api/recipes" + "?_app_key=" + params._app_key + "&_app_id=" + params._app_id + "&allowedIngredient[]=cognac");  
+        // var results =  HTTP.get("http://api.yummly.com/v1/api/recipes?_app_key=f417e2ff0264769fc557b116fb4197d0&_app_id=b1229d17&allowedIngredient[]=cognac");
+        // console.log(results);
         //result = 'test'
-        return result;
+        return results;
       } catch (e) {
         // Got a network error, time-out or HTTP error in the 400 or 500 range.
         return false;
